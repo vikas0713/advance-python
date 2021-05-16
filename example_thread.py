611808e838029d45cbf68d6eaf2d9eadebaffc02
vikas0713 <vikas0713@gmail.com>
2021-05-16 15:27:32 +0530

    Used threading and comparison with local

--- example_thread.py
+++ example_thread.py
@@ -4,11 +4,16 @@ from threading import Thread
 from time import sleep
 
 
-def countdown(n):
+def countdown(n, thread=False):
     while n > 1:
-        print("running")
+        if thread:
+            print("From Thread running")
+        else:
+            print("Plain call")
         n -= 1
-        sleep(5)
+        sleep(2)
 
 
+thread1 = Thread(target=countdown, args=(100, True))
+thread1.start()
 countdown(100)
